f291794abc493a68f7f0247557e3742027f351f9
thcheetah777 <thcheetah777@gmail.com>
2022-02-04 18:06:55 -0800

    Add pigeons

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ class Game extends Phaser.Scene {
     this.load.image("car1", "assets/car1.png");
     this.load.image("plant0", "assets/plant0.png");
     this.load.image("plant1", "assets/plant1.png");
+    this.load.image("pigeon0", "assets/pigeon0.png");
+    this.load.image("pigeon1", "assets/pigeon1.png");
+    this.load.image("poop0", "assets/poop0.png");
+    this.load.image("poop1", "assets/poop1.png");
+    this.load.image("poop2", "assets/poop2.png");
   }
   create() {
     // Create plants first layer
@@ -24,6 +29,20 @@ class Game extends Phaser.Scene {
     for (var i = 0; i < 5; i++) {
       game.plants.create(Math.random() * 2000, this.sys.game.canvas.height - 32, "plant1").setScale(8).setGravityY(-config.physics.arcade.gravity.y);
     }
+
+    // Create birds
+    game.birds = this.physics.add.group();
+    game.poop = this.physics.add.group();
+    setInterval(function () {
+      let dir = Math.floor(Math.random() * 2) === 1 ? 0 : 2000;
+      let bird = game.birds.create(dir, Math.random() * 200, "pigeon0").setScale(8).setGravityY(-config.physics.arcade.gravity.y);
+      bird.setVelocityX(dir > 0 ? -100 : 100);
+      bird.poopTimer = 100;
+      bird.dir = dir;
+      if (dir < 2000) {
+        bird.flipX = true;
+      }
+    }, Math.random() * 5000);
 
     // Input
     game.cursors = this.input.keyboard.createCursorKeys();
@@ -49,6 +68,9 @@ class Game extends Phaser.Scene {
     this.physics.add.collider(game.plants, game.plants, (plant1, plant2) => {
       plant1.destroy();
     });
+    this.physics.add.collider(game.car, game.poop, (car, poop) => {
+      
+    });
   }
   update() {
     if (game.cursors.right.isDown) {
@@ -60,5 +82,17 @@ class Game extends Phaser.Scene {
       game.car.flipX = false;
       game.car.anims.play("drive", true);
     }
+    game.birds.getChildren().forEach(bird => {
+      bird.poopTimer--;
+      console.log(bird.poopTimer);
+      if (bird.poopTimer <= 0) {
+        let poop = game.poop.create(bird.x, bird.y, "poop0").setScale(8).setSize(1, 1).setOffset(6, 6);
+        if (bird.dir < 2000) {
+          poop.flipX = true;
+          poop.setOffset(1, 6);
+        }
+        bird.poopTimer = 100;
+      }
+    });
   }
 }
