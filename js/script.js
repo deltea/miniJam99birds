efe80f94e6c3aec2f9f505892c9908769fa6c995
thcheetah777 <thcheetah777@gmail.com>
2022-02-04 17:41:42 -0800

    Add plants

--- js/script.js
+++ js/script.js
@@ -7,10 +7,23 @@ class Game extends Phaser.Scene {
   preload() {
     this.load.image("car0", "assets/car0.png");
     this.load.image("car1", "assets/car1.png");
+    this.load.image("plant0", "assets/plant0.png");
+    this.load.image("plant1", "assets/plant1.png");
   }
   create() {
+    // Create plants first layer
+    game.plants = this.physics.add.group();
+    for (var i = 0; i < 5; i++) {
+      game.plants.create(Math.random() * 2000, this.sys.game.canvas.height - 32, "plant0").setScale(8).setGravityY(-config.physics.arcade.gravity.y);
+    }
+
     // Create the car
     game.car = this.physics.add.sprite(0, 0, "car0").setScale(8).setCollideWorldBounds(true).setSize(16, 8).setOffset(0, 0).setDrag(800);
+
+    // Create plants second layer
+    for (var i = 0; i < 5; i++) {
+      game.plants.create(Math.random() * 2000, this.sys.game.canvas.height - 32, "plant1").setScale(8).setGravityY(-config.physics.arcade.gravity.y);
+    }
 
     // Input
     game.cursors = this.input.keyboard.createCursorKeys();
@@ -30,6 +43,11 @@ class Game extends Phaser.Scene {
       }],
       frameRate: 5,
       repeat: 0
+    });
+
+    // Colliders
+    this.physics.add.collider(game.plants, game.plants, (plant1, plant2) => {
+      plant1.destroy();
     });
   }
   update() {
